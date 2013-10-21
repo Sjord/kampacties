b330395b583ff0d03fe6d935a995f866009277e9
Sjoerd <sjoerd-github@linuxonly.nl>
2013-10-21 20:47:03 +0200

    Statische pagina

---
+++ www/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="../../assets/ico/favicon.png">
+
+        <title>Kampacties</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="starter-template.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <div class="container">
+            <div class="page-header">
+                    <h1>Kampacties</h1>
+            </div>
+        </div><!-- /.container -->
+
+        <div class="container">
+                <div class="jumbotron">
+                        Jelle's bruiloft &mdash; 26 oktober
+                        <div class="present">Aanwezig: Sjoerd, Els</div>
+                        <div class="absent">Afwezig: Gargamel</div>
+                        <button class="btn btn-primary add-to-action-button">Voeg jezelf toe</button>
+
+                        <form class="add-to-action-form">
+                            <table class="table">
+                                <tr>
+                                    <td>Naam:</td>
+                                    <td><input type="text" /></td>
+                                </tr>
+                                <tr>
+                                    <td>Geheime wachtwoord:</td>
+                                    <td><input type="password" /></td>
+                                </tr>
+                                <tr>
+                                    <td>Ben je er bij?</td>
+                                    <td><button class="btn btn-success">Aanwezig</button> <button class="btn btn-danger">Afwezig</button></td>
+                                </tr>
+                            </table>
+                        </form>
+                </div>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
+
