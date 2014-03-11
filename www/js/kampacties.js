d2d5ca8a0b3a9a752da7bcc95b09ddfda9a0a433
Sjoerd <sjoerd-github@linuxonly.nl>
2014-03-11 21:55:27 +0100

    Validatie op namen.

--- www/js/kampacties.js
+++ www/js/kampacties.js
@@ -1,7 +1,31 @@
 
 $('.jumbotron').each(function () {
-    var form = $(this).find('.add-to-action-form')
+    var form = $(this).find('.add-to-action-form');
     $(this).find('.add-to-action-button').click(function () {
         form.toggle();
     });
+});
+
+function valid_name(name) {
+    if (name.length < 3 || name.length > 26) {
+        return false;
+    }
+    var re = /[fkquyz]/i;
+    return !re.exec(name);
+}
+
+$("button[name='status']").click(function () {
+    var form = $(this).closest('form');
+    var input = form.find("input[name='name']");
+    var name = input.val();
+    if (valid_name(name)) {
+        return true;
+    } else {
+        input.popover({
+            content: "Vul hier gewoon je voornaam in.",
+            trigger: 'manual'
+        });
+        input.popover('show');
+        return false;
+    }
 });
